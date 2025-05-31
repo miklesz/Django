9ba7f0a8be4c8d305e9e341a1bcc55287e480994
Mikołaj Leszczuk <leszczuk@agh.edu.pl>
2025-05-31 21:10:46 +0200

    Update settings.py

--- goodmovies/settings.py
+++ goodmovies/settings.py
@@ -31,21 +31,21 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'movies',  # <-- NOWE, dopisane teraz :)
     'django.contrib.admin',
-    'django.contrib.auth',
-    'django.contrib.contenttypes',
+    'django.contrib.auth',  # <-- tu jest
+    'django.contrib.contenttypes',  # <-- i tu
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'movies',  # <-- NOWE, dopisane teraz :)
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
-    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',  # <-- tu
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
-    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',  # <-- i tu
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
